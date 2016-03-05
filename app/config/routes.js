a9f6ebe7f2595f8ccb706611a3619869521c3167
Roderick Hodgson <roderick.hodgson@googlemail.com>
2016-03-05 13:20:44 +0000

    Changed routing to use hashHistory to prevent warning

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,13 +3,14 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 
 var Main = require("../components/Main");
 var Home = require("../components/Home");
 var Undefined = null;
 
 var routes = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <IndexRoute component={Home} />
       <Route path="/undefined" component={Undefined} />
